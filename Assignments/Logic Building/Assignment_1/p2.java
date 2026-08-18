b9e6275042d18519a344c7863dd72f5db60cd4e7
sam678777 <sharmasamriddhi871@gmail.com>
2026-08-18 22:46:56 +0530

    ac_2026_assignment

--- Assignments/Logic Building/Assignment_1/p2.java
+++ Assignments/Logic Building/Assignment_1/p2.java
@@ -1,14 +1,18 @@
-class Second{
-    public static void main(String[] args){
+class Second {
+    public static void main(String[] args) {
+
         int number = -4;
-	if(number<0){
-	    System.out.println("number is negative");
- 	}
-	else if(number==0){
-	    System.out.println("number is zero");
-	}
-	else{
-	    System.out.println("number is positive");
-	}
+
+        if (number < 0) {
+            System.out.println("number is negative");
+        }
+        else {
+            if (number == 0) {
+                System.out.println("number is zero");
+            }
+            else {
+                System.out.println("number is positive");
+            }
+        }
     }
 }
