e4ebaf3b5dd4bbff222327c87234048657a2707a
sam678777 <sharmasamriddhi871@gmail.com>
2026-08-18 22:52:16 +0530

    ac_batch

--- Assignments/Logic Building/Assignment_1/p1.java
+++ Assignments/Logic Building/Assignment_1/p1.java
@@ -1,14 +1,18 @@
-class First{
-    public static void main(String[] args){
+class First {
+    public static void main(String[] args) {
+
         int number = 4;
-        if(number>0){
+
+        if (number > 0) {
             System.out.println("number is positive");
-	}
-        else if(number==0){
-            System.out.println("number is zero");
-	}
-        else{
-            System.out.println("number is negative");
-	}
+        }
+        else {
+            if (number == 0) {
+                System.out.println("number is zero");
+            }
+            else {
+                System.out.println("number is negative");
+            }
+        }
     }
 }
